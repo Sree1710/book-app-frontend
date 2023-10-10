fcac84de790451e3b9ca003bb92c02333ec426d4
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-10 18:35:21 +0530

    ViewBook component completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import AddBook from './Components/AddBook';
+import ViewBook from './Components/ViewBook';
 
 function App() {
   return (
     <div>
-      <AddBook/>
+      <ViewBook/>
     </div>
   );
 }
