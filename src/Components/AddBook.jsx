c8df7cbdbc2c46d59e95664407a0adb0eb722d0b
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-11 08:46:45 +0530

    Header component integrated with components

--- src/Components/AddBook.jsx
+++ src/Components/AddBook.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import HeaderTab from './HeaderTab'
 
 
 const AddBook = () => {
@@ -29,7 +30,7 @@ const AddBook = () => {
 
   return (
     <div>
-        
+        <HeaderTab/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
--- src/Components/SearchBook.jsx
+++ src/Components/SearchBook.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import HeaderTab from './HeaderTab'
 
 
 const SearchBook = () => {
@@ -16,7 +17,7 @@ const SearchBook = () => {
     
   return (
     <div>
-        
+        <HeaderTab/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
