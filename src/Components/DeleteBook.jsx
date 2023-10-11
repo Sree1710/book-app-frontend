4462a6c38f047d043727ef7e40cbb405eb0335e9
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-11 07:50:54 +0530

    DeleteBook frontend error rectified

--- src/Components/DeleteBook.jsx
+++ src/Components/DeleteBook.jsx
@@ -20,14 +20,14 @@ const DeleteBook = () => {
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <div className="row g-3">
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <h1>Search Books</h1>
+                                <h1>Delete Books</h1>
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <label htmlFor="" className="form-label">Book ID</label>
                                 <input onChange={inputHandler} type="text" className="form-control" name="bookId" value={inputField.bookId} />
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <button onClick={readValue} className="btn btn-warning">Search</button>
+                                <button onClick={readValue} className="btn btn-danger">Search</button>
                             </div>
                         </div>
                     </div>
